106a8105067903e5459414f6cf01c6d7edd2ff76
Het-Best <hetbest.studio@gmail.com>
2022-02-20 08:46:09 +0100

    Update 0.7.3

---
+++ JS/Games.js
@@ -0,0 +1,15 @@
+function CosmicPath_pressed() {
+  document.location.assign("https://het-best.github.io/Cosmic_path")
+}
+
+function OldIsland_pressed() {
+  document.location.assign("https://het-best.github.io/Old_island")
+}
+
+function DefuseABomb_pressed() {
+  document.location.assign("https://het-best.github.io/Defuse_a_bomb")
+}
+
+function Reblu_pressed() {
+  document.location.assign("https://het-best.github.io/Reblu")
+}
---
+++ JS/index.js
@@ -0,0 +1,15 @@
+function NewGame1_pressed() {
+  document.location.assign("https://het-best.github.io/Reblu")
+}
+
+function NewGame2_pressed() {
+  document.location.assign("https://het-best.github.io/Defuse_a_bomb")
+}
+
+function NewGame3_pressed() {
+  document.location.assign("https://het-best.github.io/Old_island")
+}
+
+function AllGames_pressed() {
+  document.location.assign("https://het-best.github.io/Games")
+}
