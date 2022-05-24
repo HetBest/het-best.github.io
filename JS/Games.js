85768e12b8973d5a8fd2dd7f08075561ea331cd5
Het-Best <hetbest.studio@gmail.com>
2022-05-24 11:24:24 +0200

    Update 0.8.0

--- JS/Games.js
+++ JS/Games.js
@@ -1,15 +1,15 @@
-function CosmicPath_pressed() {
-  document.location.assign("https://het-best.github.io/Cosmic_path")
+function to_reblu() {
+  document.location.href = "https://het-best.github.io/Reblu";
 }
 
-function OldIsland_pressed() {
-  document.location.assign("https://het-best.github.io/Old_island")
+function to_defuse_a_bomb() {
+  document.location.href = "https://het-best.github.io/Defuse_a_bomb";
 }
 
-function DefuseABomb_pressed() {
-  document.location.assign("https://het-best.github.io/Defuse_a_bomb")
+function to_old_island() {
+  document.location.href = "https://het-best.github.io/Old_island";
 }
 
-function Reblu_pressed() {
-  document.location.assign("https://het-best.github.io/Reblu")
+function to_cosmic_path() {
+  document.location.href = "https://het-best.github.io/Cosmic_path";
 }
--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,3 @@
-function NewGame1_pressed() {
-  document.location.assign("https://het-best.github.io/Reblu")
-}
-
-function NewGame2_pressed() {
-  document.location.assign("https://het-best.github.io/Defuse_a_bomb")
-}
-
-function NewGame3_pressed() {
-  document.location.assign("https://het-best.github.io/Old_island")
-}
-
-function AllGames_pressed() {
-  document.location.assign("https://het-best.github.io/games")
+function to_last_game() {
+  document.location.href = "https://het-best.github.io/reblu";
 }
